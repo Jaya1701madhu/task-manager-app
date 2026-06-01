0efb10759f9071d90f34232f58162d39dd961d05
Jaya1701madhu <jayamdhu1701@gmail.com>
2026-06-01 14:43:01 +0530

    fix api import issue

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import API from "../services/api";
-
+import API from "../services/api";
 function Login() {
   const navigate = useNavigate();
 
