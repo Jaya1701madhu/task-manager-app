5619b0ec47e4b79f04534b436a87536fd66a2ef9
Jaya1701madhu <jayamdhu1701@gmail.com>
2026-06-01 14:13:43 +0530

    fix render deployment issues

--- server/server.js
+++ server/server.js
@@ -8,19 +8,28 @@ const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
+
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
 
-    app.listen(5000, () => {
-      console.log("Server Running On Port 5000");
+    app.listen(PORT, () => {
+      console.log(`Server Running On Port ${PORT}`);
     });
   })
   .catch((err) => console.log(err));
