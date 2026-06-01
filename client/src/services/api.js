ea749ffda55e16c22f34dc0650605e3d2c695059
Jaya1701madhu <jayamdhu1701@gmail.com>
2026-06-01 15:01:30 +0530

    force api routing fix

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-manager-app-chl9.onrender.com",
+  baseURL: "https://task-manager-app-chl9.onrender.com/api",
 });
 
 export default API;
