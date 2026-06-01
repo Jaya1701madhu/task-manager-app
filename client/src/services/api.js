57740f4cd720814fe127ab9f8dd1e20bcc020696
Jaya1701madhu <jayamdhu1701@gmail.com>
2026-06-01 14:52:12 +0530

    check again

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-manager-app-chl9.onrender.com/api",
+  baseURL: "https://task-manager-app-chl9.onrender.com",
 });
 
 export default API;
